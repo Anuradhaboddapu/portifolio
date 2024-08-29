cfb6c0794e37a49edbedc78eac444ebb293e9859
anuradha <b.anuradha7729@gmail.com>
2024-08-29 22:00:19 +0530

    ok

--- darkMode.js
+++ darkMode.js
@@ -12,16 +12,16 @@ function toggleDarkMode() {
     // Toggle the 'dark-mode' class on the body
     document.body.classList.toggle('dark-mode');
 
-    // Change the icon based on the mode
+   
     if (document.body.classList.contains('dark-mode')) {
         darkModeIcon.classList.replace('bx-moon', 'bx-sun');
     } else {
         darkModeIcon.classList.replace('bx-sun', 'bx-moon');
     }
 
-    // Save the user's preference to local storage
+   
     localStorage.setItem('darkMode', document.body.classList.contains('dark-mode'));
 }
 
-// Event listener for the dark mode icon click
+
 darkModeIcon.addEventListener('click', toggleDarkMode);
